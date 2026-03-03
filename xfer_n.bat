28c6c7e1633f4ccdf07c0d9c10be3d419459d774
ontheslab <serverzaphod@gmail.com>
2026-03-03 23:34:16 +1100

    First upload

---
+++ xfer_n.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo.
+echo Transfer files to NABU/TRS80 'D' Drive for testing (D:\NIA\NABU Internet Adapter\Store\D\1)
+echo ****************************************************************************
+
+copy *.com "D:\NIA\NABU Internet Adapter\Store\D\1\"
+
+echo ****************************************************************************
+
+copy *.com "D:\Retro\TRS80\frehd"
+
+echo ****************************************************************************
+
+copy *.com "P:\Retro\TRS80\frehd"
+
+echo ****************************************************************************
+pause
